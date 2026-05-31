014c208b090db26cdc2843c41f21c41f319fb5c6
Waza <wazhawasti@gmail.com>
2026-05-31 06:21:54 +0600

    Update Main_Code.py

--- Main_Code.py
+++ Main_Code.py
@@ -2,7 +2,6 @@ name=input ("May I know your name? ")
 print ("Hello," ,name+ """. Welcome to Xenon's Caffee!
 I am an Autonomous Coffee Barista.
 Sir, do you want the menu for today?""")
-
 confirm= input ("Yes/No: ")
 if confirm.lower() == "yes":
     print ("""Here is our Menu sir:
@@ -12,18 +11,13 @@ Hot Coffee          Cold Coffee            Tea               Dessert
 else:
     print ("Thank You Sir, Visit Us Again!")
     exit ()
-
 more_menu= input ("Which one do you want? ")
 if more_menu.lower()=="hot coffee":
     print ("Name:",name,"Order: One Hot Coffee.","Everthing Okay Sir?")
-
-
 elif more_menu.lower()=="cold coffee":
     print ("Name:", name,"Order: One Cold Coffee.","Everthing Okay Sir?")
-
 elif more_menu.lower()=="tea":
     print ("Name:", name,"Order: One Tea.","Everthing Okay Sir?")
-
 elif more_menu.lower()=="dessert":
     print ("Name:", name,"Order: One Dessert.","Everthing Okay Sir?")
 else:
